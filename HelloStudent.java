4987c14c9af46291105e88d0e218cfc4654a099b
anmol-pimpale <anmolpimpale90@gmail.com>
2025-05-07 19:35:41 +0530

    java file

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
+
 public class HelloStudent  {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-
-        scanner.close();
+        System.out.println("Hello,  Anmol" );
     }
 }
